1da99389dcd53444b048a964aceee3db8bf0fc65
nikaa <n.kakauridze9@gmail.com>
2026-05-31 14:03:01 +0400

    moved lisene and readme

--- main.cpp
+++ main.cpp
@@ -1,25 +1,13 @@
-#include "APIs/Headers/DBAPI.h"
-#include <thread>
-#include <curl/curl.h>
+#include "GUI/GUI.h"
 
-#include "Services/Headers/Session.h"
-
-void Pause() {
-
-    // 3. Pause the program for exactly 1 second
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-
-}
-
-Session bootstrap;
+Session Session_Bootstrap;
+GUI GUI_Bootstrap;
 
 int main() {
-    Base::FindGroup(5).FindRootChat(28).FocusChat();
-    Session::chatPtr->LoadTree();
-    Session::chatPtr->PrintData();
-    Session::chatPtr->CreateBranch("Branch2");
-    Session::chatPtr->SendPrompt("what was your third response to me?");
-    Session::chatPtr->PrintData();
-    // Tree::PrintData();
+    if (!GUI::Initialize(1280, 720, "Branchat App")) {
+        return -1;
+    }
 
+   GUI::StartRenderLoop();
+   return 0;
 }
